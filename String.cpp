5228e4cddfde74845376dd055c371f51ef3d50f1
john tau <john.tau22@gmail.com>
2017-10-27 18:48:23 +0300

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -3,7 +3,7 @@
 #include <ostream>
 #include <cstring>
 
-size_t slen(const char*);
+size_t s_len(const char*);
 
 class String
 {
@@ -12,10 +12,10 @@ class String
 
 	char* resize(char*, const size_t);
 
-	friend size_t slen(const char*);
+	friend size_t s_len(const char*);
 	
 public:
-	String() :s{ new char[] }, len{ 0 } {};
+	String() :s{ new char[0] }, len{ 0 } {};
 	String(char*);
 	String(int);
 	String(double);
@@ -35,29 +35,32 @@ public:
 
 String::String(char* str)
 {
-	s = new char[];
+	int new_len = s_len(str);
+	s = new char[new_len];
 	s = str;
-	len = slen(str);
+	len = new_len;
 }
 
 String::String(int a)
 {
-	s = new char[];
 	const int blen = 100;
 	char buff[blen];
 	sprintf_s(buff, blen, "%d", a);
+	int new_len = s_len(buff);
+	s = new char[new_len];
 	s = buff;
-	len = slen(s);
+	len = new_len;
 }
 
 String::String(double d)
 {
-	s = new char[];
 	const int blen = 100;
 	char buff[blen];
 	sprintf_s(buff, blen, "%f", d);
+	int new_len = s_len(buff);
+	s = new char[new_len];
 	s = buff;
-	len = slen(s);
+	len = new_len;
 }
 
 char* String::resize(char* oldstr, const size_t sz)
@@ -70,7 +73,12 @@ char* String::resize(char* oldstr, const size_t sz)
 	else
 	{
 		char* newstr = new char[sz];
-		*newstr = *oldstr;
+		for (int i = 0; i < len; ++i)
+		{
+			newstr[i] = oldstr[i];
+		}
+		newstr[len] = '\0';
+
 		delete oldstr;
 		return newstr;
 	}
@@ -96,8 +104,11 @@ char String::operator[](const int i) const
 String& String::operator+(String& other)
 {
 	char buff[1024];
-	buff = this->s;
-	buff = 1;
+	strcpy_s(buff, (len+1) * sizeof(char), this->s);
+	strcpy_s(buff+(len+1), (s_len(other.s) + 1) * sizeof(char), other.s);
+	String NewStr(buff);
+
+	return NewStr;
 }
 
 String& String::operator+=(String& other)
@@ -105,11 +116,13 @@ String& String::operator+=(String& other)
 	;
 }
 
+// copy assignment
 String& String::operator=(String& other)
 {
 	;
 }
 
+// move assignment
 String& String::operator=(String&& other)
 {
 	;
@@ -117,7 +130,10 @@ String& String::operator=(String&& other)
 
 String& String::SubStr(int i, size_t sz) const
 {
-	;
+	String NewStr(14);
+	NewStr.resize();
+	for (int j = 0; j < sz; ++j)
+		NewStr.s[j] = this->s[i + j];
 }
 
 void String::WriteFile(std::ofstream& os)
@@ -132,9 +148,9 @@ String& String::ReadFile(std::ifstream& is) const
 
 
 
-// TO_REMOVE: keep slen at the end
+// TO_REMOVE: keep s_len at the end
 
-size_t slen(const char* s)
+size_t s_len(const char* s)
 {
 	if (s[0] == '\0')
 	{
