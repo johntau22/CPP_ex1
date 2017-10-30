d1492a1561d5b063c23959e9303f285993b4175b
john tau <john.tau22@gmail.com>
2017-10-30 19:24:43 +0200

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -2,39 +2,46 @@
 #include <fstream>
 #include <ostream>
 #include <cstring>
+#include <memory>
 #pragma warning(disable : 4996)
 
 using namespace std;
 
 class String {
-	char* _s;
-	size_t _size;
-
-	char* _resize(char*, const size_t);
-	friend size_t s_len(const char*);
-	friend int compare(const String&, const String&);
-	friend String& SubStr(const String&, const int, const size_t);
-	friend String& operator+(String&, const String&);
-	friend istream& operator>>(istream&, String&);
-	friend ostream& operator<<(ostream&, const String&);
 
 public:
 	String()
-		:_s{ new char[1] }, _size{ 0 }	{ _s[0] = '\0'; };
+		:_s{ new char[1] }, _size{ 0 } { _s[0] = '\0'; };
 	String(const char*);
 	String(istream&);
 	String(int);
 	String(double);
-	String(const String&);
-	String(String&&);
+	String(const String&);	// copy constructor
+	String(String&&);	// move constructor
 	~String() { delete[] _s; };
 
 	char operator[](const int) const;
-	String& operator+=(String&);
-	String& operator=(const String&);
-	String& operator=(String&&);
-	void WriteFile(ofstream&) const;
-	String& ReadFile(ifstream&);
+	bool operator< (const String&) const;
+	bool operator> (const String&) const;
+	bool operator== (const String&) const;
+	bool operator!= (const String&) const;
+	String& operator+= (String&);
+	String& operator= (const String&);	// copy assignment
+	String& operator= (String&&);	// move assignment
+	void WriteFile(char*) const;
+	String ReadFile(char*);
+
+	friend String operator+ (const String&, const String&);
+	friend String SubStr(const String&, const int, const size_t);
+	friend istream& operator >> (istream&, String&);
+	friend ostream& operator<< (ostream&, const String&);
+
+private:
+	char* _s;
+	size_t _size;
+
+	String& _resize(const size_t);
+	friend size_t s_len(const char*);
 };
 
 int main()
@@ -44,23 +51,27 @@ int main()
 	String A("hello");
 	String B(a);
 	String C(d);
-	String D(C);
+	String D(B);
 	String E;
-	E = D + A;
+	E = B;
+	String F;
+	F = B + A;
 	E += D;
-	String F(cin);
+	String G(cin);
 
-	cout << A;
+	cout << "A: \t\t" << A;
 	cout << endl;
-	cout << B;
+	cout << "B: \t\t" << B;
 	cout << endl;
-	cout << C;
+	cout << "C: \t\t" << C;
 	cout << endl;
-	cout << D;
+	cout << "D (B): \t\t" << D;
 	cout << endl;
-	cout << E;
+	cout << "E=B -> E+=D: \t" << E;
 	cout << endl;
-	cout << F;
+	cout << "F (B + A): \t" << F;
+	cout << endl;
+	cout << "G: \t\t" << G;
 	cout << endl;
 }
 
@@ -84,7 +95,7 @@ String::String(int a)
 {
 	const int b_len = 64;
 	char buff[b_len];
-	sprintf(buff, "%d", a);
+	snprintf(buff, b_len, "%d", a);
 	_size = s_len(buff);
 	_s = new char[_size + 1];
 	strcpy(_s, buff);
@@ -94,7 +105,7 @@ String::String(double d)
 {
 	const int b_len = 64;
 	char buff[b_len];
-	sprintf(buff, "%0.15f", d);
+	snprintf(buff, b_len, "%0.15f", d);
 	_size = s_len(buff);
 	_s = new char[_size + 1];
 	strcpy(_s, buff);
@@ -102,76 +113,107 @@ String::String(double d)
 
 // copy constructor
 String::String(const String& other)
-	:_s{ new char[s_len(other._s)] }, _size{ s_len(other._s) }
+	:_s{ new char[s_len(other._s) + 1] }, _size{ s_len(other._s) }
 {
 	strcpy(_s, other._s);
 }
 
 // move constructor
 String::String(String&& other)
-	:_s{ other._s }, _size{ other._size}
+	:_s{ other._s }, _size{ other._size }
 {
 	other._s = nullptr;
 }
 
-char* String::_resize(char* oldstr, const size_t sz)
+String& String::_resize(const size_t sz)
 {
 	if (sz <= _size)
 	{
 		cerr << "New size is not bigger than old, no resize needed." << endl;
-		return oldstr;
+		return *this;
 	}
 	else
 	{
-		char* newstr = new char[sz];
-		strcpy(newstr, oldstr);
-		delete[] oldstr;
-		return newstr;
+		char* new_s = new char[sz];
+		strcpy(new_s, _s);
+		char* old_s = _s;
+		this->_s = new_s;
+		delete[] old_s;
+		return *this;
 	}
 }
 
-char String::operator[](const int i) const
+char String::operator[] (const int i) const
 {
 	if (i >= 0 && i < _size)
-	{
 		return _s[i];
-	}
 	else
-	{
-		cerr << "Out of bounds." << endl;
-	}
+		throw out_of_range{"String::operator[]"};
 }
 
-istream& operator>>(istream& is, String& S)
+bool String::operator< (const String& other) const
+{
+	if (strcmp(_s, other._s) < 0)
+		return true;
+	else
+		return false;
+}
+
+bool String::operator> (const String& other) const
+{
+	if (strcmp(_s, other._s) > 0)
+		return true;
+	else
+		return false;;
+}
+
+bool String::operator== (const String& other) const
+{
+	if (strcmp(_s, other._s) == 0)
+		return true;
+	else
+		return false;;
+}
+
+bool String::operator!= (const String& other) const
+{
+	if (strcmp(_s, other._s) != 0)
+		return true;
+	else
+		return false;;
+}
+
+istream& operator>> (istream& is, String& S)
 {
 	is >> S._s;
 	return is;
 }
 
-ostream& operator<<(ostream& os, const String& S)
+ostream& operator<< (ostream& os, const String& S)
 {
 	os << S._s;
 	return os;
 }
 
-String& String::operator+=(String& other)
+String& String::operator+= (String& other)
 {
 	int oth_len = s_len(other._s);
 	int new_len = _size + oth_len;
-	_s = _resize(_s, new_len + 1);
+	_resize(new_len + 1);
 	memcpy(&_s[_size], other._s, (oth_len + 1) * sizeof(char));
+	_size = s_len(_s);
 
 	return *this;
 }
 
 // copy assignment
-String& String::operator=(const String& other)
+String& String::operator= (const String& other)
 {
 	if (this != &other)
 	{
-		_s = new char[s_len(other._s + 1)];
-		_size = s_len(_s);
+		_s = new char[s_len(other._s) + 1];
 		strcpy(_s, other._s);
+		_size = s_len(_s);
 	}
 	else
 	{
@@ -182,13 +224,14 @@ String& String::operator=(const String& other)
 }
 
 // move assignment
-String& String::operator=(String&& other)
+String& String::operator= (String&& other)
 {
 	if (this != &other)
 	{
 		delete[] _s;
 		_s = other._s;
 		other._s = nullptr;
+		_size = other._size;
 	}
 	else
 	{
@@ -198,15 +241,35 @@ String& String::operator=(String&& other)
 	return *this;
 }
 
-void String::WriteFile(ofstream& ofs) const
+void String::WriteFile(char* name) const
 {
-	ofs << this->_s;
+	FILE* pFile = fopen(name, "w");
+	if (pFile != NULL)
+	{
+		fputs(_s, pFile);
+		fclose(pFile);
+	}
+	else
+	{
+		cout << "Error opening file." << endl;
+	}
 }
 
-String& String::ReadFile(ifstream& ifs)
+String String::ReadFile(char* name)
 {
-	ifs >> this->_s;
-	return *this;
+	FILE* pFile = fopen(name, "r");
+	if (pFile != NULL)
+	{
+		char s[256];
+		fgets(s, 256, pFile);
+		String NewStr(s);
+
+		return NewStr;	// new object by value
+	}
+	else
+	{
+		cout << "Error opening file." << endl;
+	}
 }
 
 size_t s_len(const char* s)
@@ -223,34 +286,26 @@ size_t s_len(const char* s)
 	return len;
 }
 
-int compare(const String& str1, const String& str2)
-{
-	return strcmp(str1._s, str2._s);
-}
-
-String& SubStr(const String& str, const int i, const size_t sz)
+String SubStr (const String& str, const int i, const size_t sz)
 {
 	char* s = new char[sz + 1];
-	strcpy(s, &(str._s[i]) );
+	strcpy(s, &(str._s[i]));
 	s[sz] = '\0';
 	String SubStr(s);
 	delete[] s;
 
-	return SubStr;
+	return SubStr;	// new object by value
 }
 
-
-String& operator+(String& Dest, const String& Src)
+String operator+ (const String& Str1, const String& Str2)
 {
-	int src_len = s_len(Src._s);
-	int new_len = Dest._size + src_len;
+	int oth_len = s_len(Str2._s);
+	int new_len = Str1._size + oth_len;
 	char* buff = new char[new_len + 1];
-	memcpy(buff, Dest._s, Dest._size * sizeof(char));
-	memcpy(&buff[Dest._size], Src._s, (src_len + 1) * sizeof(char));	// copy with null-terminator
-
-	Dest._resize(Dest._s, new_len + 1);
-	strcpy(Dest._s, buff);
+	memcpy(buff, Str1._s, Str1._size * sizeof(char));
+	memcpy(&buff[Str1._size], Str2._s, (oth_len + 1) * sizeof(char));	// copy with null-terminator
+	String NewStr(buff);
 	delete[] buff;
 
-	return Dest;
+	return NewStr;	// new object by value
 }
